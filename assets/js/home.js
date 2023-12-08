8f58233850e0c2f857d518823cbdfe2a1ff7bdbc
Hugo <rytlewski.hugo@etu.educ-valadon-limoges.fr>
2023-12-08 03:10:05 +0100

    push

--- assets/js/home.js
+++ assets/js/home.js
@@ -6,14 +6,12 @@ function openMenu() {
 
     if (!IsOpen) {
         dropdown.style.display = 'block';
-        dropdown.style.animation = 'dropdownAnimation 0.5s ease';
         IsOpen = true;
     } else {
         dropdown.classList.remove('open');
-        dropdown.style.animation = 'reversedropdownAnimation 0.5s ease-in';
-        setTimeout(() => {
+       
             dropdown.style.display = 'none';
-        }, 490);
+        
         IsOpen = false;
     }
 
