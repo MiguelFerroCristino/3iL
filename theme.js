352c587420049604a91e53cc80df681220c2ef82
LouisHM <louismaury19@gmail.com>
2023-12-08 04:29:47 +0100

    Add setTheme function to home.js and update index.html

--- theme.js
+++
@@ -1,8 +0,0 @@
-function setTheme(theme) {
-    // Supprimer toutes les classes de thème du body
-    document.body.classList.remove('dark-mode', 'protonopia', 'deuteranopia', 'tritanopia');
-  
-    // Ajouter la classe du thème sélectionné au body
-    document.body.classList.add(theme);
-  }
-  
