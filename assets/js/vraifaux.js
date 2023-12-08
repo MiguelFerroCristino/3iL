1b0d300c07f782fd368c49cf736fd32bd3095d1f
Hugo <rytlewski.hugo@etu.educ-valadon-limoges.fr>
2023-12-08 03:49:00 +0100

    robin push

---
+++ assets/js/vraifaux.js
@@ -0,0 +1,87 @@
+var questionsList = [
+    {
+        question: "Les énergies renouvelables sont plus chères à produire.",
+        reponse: false,
+        explication: "Les progrès technologiques dans le domaine ont entraîné une baisse spectaculaire des coûts des énergies renouvelables. Ainsi, actuellement, que ce soit n'importe où dans le monde, aucune autre source de production électrique n'est aussi économiquement compétitive que l'énergie éolienne ou solaire pour de nouvelles installations."
+    },
+    {
+        question: "Adopter un mode de vie écolo, ce serait revenir au Moyen Âge.",
+        reponse: false,
+        explication: "Le mode de vie écologique repose sur un ensemble d'innovations et d'améliorations du bien-être, qui relève d’un processus de progrès… et certainement pas d’un retour en arrière !"
+    },
+    {
+        question: "Manger de la viande est mauvais pour l'environnement.",
+        reponse: true,
+        explication: "Il faut 30.000 à 60.000 litres d'eau pour faire 1 kg de viande de bœuf."
+    },
+    {
+        question: "Les panneaux solaires ne se valent pas tous.",
+        reponse: true,
+        explication: "Les panneaux solaires, en fonction de leur matière, peuvent avoir un rendement de 5 à 19% !"
+    },
+    {
+        question: "Les panneaux solaires ne sont pas rentable du fait d'une durée de vie trop courte.",
+        reponse: false,
+        explication: "Les panneaux en eux-mêmes ont une durée de vie comprise entre 20 et 35 ans suivant les modèles."
+    },
+    {
+        question: "Un produit sans sucre contient tout de même du sucre.",
+        reponse: true,
+        explication: "Selon la législation européenne, un produit portant la mention « sans sucre » peut contenir jusqu’à 0,5 g/100 de sucres, en plus de contenir des \"faux sucres\" ou édulcorants"
+    },
+    {
+        question: "Un feu de cheminée ne pollue pas.",
+        reponse: false,
+        explication: "En plus de polluer l'air exterieur, les particules fines dégagées par les feux de cheminées polluent également l'intérieur de notre maison."
+    },
+    {
+        question: "Les sacs en plastique fin sont aujourd'hui tous biodégradables.",
+        reponse: false,
+        explication: "Dans la nature, le sac en plastique mettra très longtemps à se dégrader et aura de graves conséquences pour la faune et la flore. Il n'est également pas compostable."
+    },
+    {
+        question: "Avec un lave-linge de grande capacité, on fait moins de lessives.",
+        reponse: false,
+        explication: "Même performant, un lave-linge grande capacité, s'il ne convient pas à vos besoin tournera souvent sans être totalement remplies, entraînant un gaspillage d'énergies et d'eau."
+    },
+    {
+        question: "Les ampoules basse consommation ne s'adaptent pas à toutes les lampes et ne supportent pas les allumages répétés.",
+        reponse: false,
+        explication: "Les ampoules basse consommation s'adaptent à de nombreux luminaires, et certaines d'entre elles sont spécifiquement adaptés aux allumages fréquents.  "
+    },
+];
+
+function setQuestion(questionIndex) {
+    var questionElement = document.getElementById('question');
+    questionElement.textContent = questionsList[questionIndex].question;
+}
+
+function cancelFenetre() {
+    var el = document.getElementById('resultat');
+    el.style.animation = "fadeUpOut 0.5s ease-in-out";
+    setTimeout(function () {
+        el.style.display = "none";
+    }, 500);
+}
+
+function checkAnswer(userAnswer) {
+    var currentQuestionIndex = 0;
+    var correctAnswer = questionsList[currentQuestionIndex].reponse;
+
+    var comparatifElement = document.getElementById('comparatif');
+    var resultatElement = document.getElementById('resultat');
+
+    if (userAnswer === correctAnswer) {
+        comparatifElement.textContent = "Bonne réponse!";
+        resultatElement.style.backgroundColor = 'green';
+    } else {
+        comparatifElement.textContent = "Mauvaise réponse!";
+        resultatElement.style.backgroundColor = 'red';
+    }
+
+    resultatElement.style.display = 'flex';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    setQuestion(0);
+});
