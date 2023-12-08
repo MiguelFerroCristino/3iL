e2681603f397deb21ea7d643df99f244971d3588
Hugo <rytlewski.hugo@etu.educ-valadon-limoges.fr>
2023-12-08 05:02:19 +0100

    importantpush

--- homepage/home.js
+++ homepage/home.js
@@ -1,11 +1,32 @@
 let IsOpen = false;
+let IsOpen2 = false;
+
+const menu2 = document.getElementById('menu2');
 const menu = document.getElementById('menu');
+
+function openMenu2() {
+    const dropdown2 = document.querySelector('.dropdown-acces2');
+
+    if (!IsOpen2) {
+        dropdown2.style.display = 'block';
+        
+        IsOpen2 = true;
+    } else {
+        dropdown2.classList.remove('open');
+       
+            dropdown2.style.display = 'none';
+        
+        IsOpen2 = false;
+    }
+
+}
 
 function openMenu() {
     const dropdown = document.querySelector('.dropdown-acces');
 
     if (!IsOpen) {
         dropdown.style.display = 'block';
+        
         IsOpen = true;
     } else {
         dropdown.classList.remove('open');
