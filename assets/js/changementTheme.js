967b13ceb3a47e2f0a6605dafbe34461df9b0643
MatisRault <rault.matis@etu.educ-valadon-limoges.fr>
2023-12-08 03:33:24 +0100

    sfdzad

--- assets/js/changementTheme.js
+++ assets/js/changementTheme.js
@@ -1,6 +1,6 @@
 function setTheme(theme) {
     // Supprimer toutes les classes de thème du body
-    document.body.classList.remove('dark-mode', 'protonopia', 'deuteranopia', 'tritanopia');
+    document.body.classList.remove('dark-mode', 'protonopia', 'deuteranopia', 'tritanopia', 'dyslexia');
   
     // Ajouter la classe du thème sélectionné au body
     document.body.classList.add(theme);
